425c9529f47a7d350f125a159998ab056cfa3210
sakshithange7-sketch <sakshithange7@gmail.com>
2026-04-19 19:35:17 +0530

    Week 3: Added frontend templates using Jinja2

--- app.py
+++ app.py
@@ -1,57 +1,35 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 from forms import TextQuestionForm, MCQForm
 
+# Create Flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
 
-# Secret key for forms
-app.config['SECRET_KEY'] = 'secret123'
-
-# Database config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
-
-db = SQLAlchemy(app)
-
-# ---------------- MODELS ----------------
-
-class Quiz(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
-    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
-
-class Choice(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(100))
-    is_correct = db.Column(db.Boolean)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-
-# ---------------- ROUTES ----------------
-
+# Home route
 @app.route('/')
 def home():
-    return "Quiz Backend Running"
+    return '''
+    <h2>Welcome to Quiz App</h2>
+    <a href="/text">Text Question</a><br><br>
+    <a href="/mcq">MCQ Question</a>
+    '''
 
+# Text question route
 @app.route('/text', methods=['GET', 'POST'])
 def text_question():
     form = TextQuestionForm()
     if form.validate_on_submit():
         return f"You answered: {form.question.data}"
-    return str(form.question.label) + str(form.question) + str(form.submit)
+    return render_template('text.html', form=form)
 
+# MCQ route
 @app.route('/mcq', methods=['GET', 'POST'])
 def mcq_question():
     form = MCQForm()
     if form.validate_on_submit():
         return f"You selected: {form.answer.data}"
-    return str(form.answer.label) + str(form.answer) + str(form.submit)
+    return render_template('mcq.html', form=form)
 
-# ---------------- RUN ----------------
-
+# Run app
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
